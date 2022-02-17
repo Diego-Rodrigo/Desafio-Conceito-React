398644a9cfa76dd6f5c103ea5fcb9b6269079523
Diego Rodrigo <diego.rodrigo.santos@outlook.com.br>
2022-02-17 17:41:00 -0300

    tada: Desafio Finalizado

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,14 +16,32 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if(!newTaskTitle) return;
+
+    const newTask = {
+      id: Math.random(),
+      title: newTaskTitle,
+      isComplete: false
+    }
+
+    setTasks(oldState => [...oldState, newTask]);
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const alterTask = tasks.map(task => task.id === id ? {
+      ...task,
+      isComplete: !task.isComplete
+    } : task)
+
+    setTasks(alterTask)
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    const filterAllTask = tasks.filter(task => task.id !== id)
+
+    setTasks(filterAllTask)
   }
 
   return (
